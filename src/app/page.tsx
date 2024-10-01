bd7dd595a8be0dc4e7797d15ef2967e8e70b9da2
AmanAta <atamuhammadaman@gmail.com>
2024-10-01 23:29:15 +0530

    name change

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,7 +10,7 @@ export default function Home() {
       <div className="hero-overlay bg-opacity-60"></div>
       <div className="hero-content text-neutral-content text-center">
         <div className="max-w-md">
-          <h1 className="mb-5 text-5xl font-bold">Hello suvam</h1>
+          <h1 className="mb-5 text-5xl font-bold">Hello Aman</h1>
           <p className="mb-5">
             Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
             excepturi exercitationem quasi. In deleniti eaque aut repudiandae et
