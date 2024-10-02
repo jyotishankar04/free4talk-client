3845ed73cad5b9dc19eb47616d8cb6626f8391c6
AmanAta <atamuhammadaman@gmail.com>
2024-10-02 18:55:45 +0530

    Components created

---
+++ src/components/Home/home.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+function Home() {
+  return (
+    <div className="hero min-h-screen">
+    <div className="hero-overlay bg-opacity-60"></div>
+    <div className="hero-content text-neutral-content text-center">
+      <div className="max-w-md">
+        <h1 className="mb-5 text-5xl font-bold">Free4talk</h1>
+        <p className="mb-5">
+          Testing by aman
+        </p>
+        <button className="btn btn-primary">Get Started</button>
+      </div>
+    </div>
+  </div>
+  )
+}
+
+export default Home
